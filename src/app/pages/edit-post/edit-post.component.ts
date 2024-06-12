b8b40403d6fe4466cbbd1bfc6830e34398e8a11d
Shreyansh Saagar <shreyansh@tritv65.com>
2024-06-12 19:56:32 +0530

    new changes on web

--- src/app/pages/edit-post/edit-post.component.ts
+++ src/app/pages/edit-post/edit-post.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { HeaderComponent } from '../../shared-component/header/header.component';
-import { FormsModule, NgForm } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PostService } from '../../post.service';
@@ -9,39 +9,64 @@ import { PostService } from '../../post.service';
 @Component({
   selector: 'app-edit-post',
   standalone: true,
-  imports: [CommonModule,HeaderComponent,FormsModule],
+  imports: [CommonModule, HeaderComponent, ReactiveFormsModule],
   templateUrl: './edit-post.component.html',
-  styleUrl: './edit-post.component.css'
+  styleUrls: ['./edit-post.component.css']
 })
 export class EditPostComponent implements OnInit {
-  auth = inject(AuthService)
-  router = inject(ActivatedRoute)
-  post = inject(PostService)
-  route = inject(Router)
-  postId:any
-  @ViewChild('editPost') edit!: NgForm
+  auth = inject(AuthService);
+  router = inject(ActivatedRoute);
+  post = inject(PostService);
+  route = inject(Router);
+  postId: any;
+  editForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
   ngOnInit(): void {
-      this.router.paramMap.subscribe((params)=>{
-        const postId = params.get('id')
-        this.postId = postId
-        this.post.getSinglePost(postId).subscribe({
-          next:(res)=>{
-            this.post.singlePost=Object.values(res)[0]
-          },error:()=>{
-            console.log('error edit post');
-          }
-        })
-      })
+    this.editForm = this.fb.group({
+      title: ['', Validators.required],
+      desc: ['', Validators.required],
+      related: ['', Validators.required],
+      topics: ['', Validators.required]
+    });
+
+    this.router.paramMap.subscribe((params) => {
+      const postId = params.get('id');
+      this.postId = postId;
+      this.post.getSinglePost(postId).subscribe({
+        next: (res) => {
+          const post = Object.values(res)[0];
+          this.editForm.patchValue({
+            title: post.title,
+            desc: post.desc,
+            related: post.related,
+            topics: post.topics
+          });
+        },
+        error: () => {
+          console.log('error fetching post');
+        }
+      });
+    });
   }
 
-  editForm(){
+  onSubmit() {
     const formData = new FormData();
-    formData.append('title',this.edit.value.title)
-    formData.append('desc',this.edit.value.desc)
-    formData.append('related',this.edit.value.related)
-    formData.append('topics',this.edit.value.topics)
-    this.post.editPost(this.postId,formData);
-    this.edit.resetForm();
-    // this.route.navigate(['/']).then(()=>window.location.reload())
+    formData.append('title', this.editForm.get('title')?.value);
+    formData.append('desc', this.editForm.get('desc')?.value);
+    formData.append('related', this.editForm.get('related')?.value);
+    formData.append('topics', this.editForm.get('topics')?.value);
+
+    this.post.editPost(this.postId, formData).subscribe({
+      next: (res) => {
+        console.log('Update successful', res);
+        // this.route.navigate(['/']);  // Optionally navigate to another page
+      },
+      error: (err) => {
+        console.error('Error updating post', err);
+        // Optionally show an error message
+      }
+    });
   }
 }
--- src/app/shared-component/detail-blog/detail-blog.component.ts
+++ src/app/shared-component/detail-blog/detail-blog.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { posts } from '../../../posts.model';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../auth.service';
 import { PostService } from '../../post.service';
 
@@ -14,13 +14,14 @@ import { PostService } from '../../post.service';
 })
 export class DetailBlogComponent implements OnInit{
 
-  route=inject(ActivatedRoute)
+  router=inject(ActivatedRoute)
+  route=inject(Router)
   auth=inject(AuthService)
   post = inject(PostService)
   postDetail:any;
   error:any;
   ngOnInit(): void {
-      this.route.paramMap.subscribe((params)=>{
+      this.router.paramMap.subscribe((params)=>{
         const postId = params.get('id');
         this.post.getSinglePost(postId).subscribe({
           next:(res)=>{
@@ -34,4 +35,19 @@ export class DetailBlogComponent implements OnInit{
       })
   }
 
+
+
+  deletePost(postId: string) {
+    if (confirm('Are you sure you want to delete this post?')) {
+      this.post.deletePost(postId).subscribe({
+        next: (res) => {
+          console.log('Delete successful', res);
+          this.route.navigate(['/blog']); // Navigate to another page after deletion
+        },
+        error: (err) => {
+          console.error('Error deleting post', err);
+        }
+      });
+    }
+  }
 }
